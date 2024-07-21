2613657d6fb5f02af3dd0e1fa39ca6e4dbf4059b
dimas-fahmi <dimasfahmipebrian@gmail.com>
2024-07-21 09:50:07 +0700

    rewrite the code with clsx and add some animation

--- src/components/GameBoard/fragments/GameBoardBox.jsx
+++ src/components/GameBoard/fragments/GameBoardBox.jsx
@@ -1,18 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import clsx from "clsx";
 import useGameContext from "../../../lib/hooks/useGameContext";
 import { ACTIONS } from "../../../lib/actions";
-import { RiDeleteBin7Fill } from "react-icons/ri";
+import { IoMdMove } from "react-icons/io";
 
+// GameBoardBox component that represents a single box in the game board
 function GameBoardBox({ boxId, boxHolder }) {
-  // GET dispatch, xNext, winner from GameContext
-  const { xNext, winner, dispatch, moves, helper, boardState } =
-    useGameContext();
+  // Destructure needed values from the game context
+  const { xNext, winner, dispatch, moves, helper } = useGameContext();
 
-  // Helper State | Get from last moves of each player
+  // Get the last moves from both players
   const lastX = moves.x[2];
   const lastO = moves.o[2];
 
-  // dispatch handler for Advancing the Game (push the new holder to state)
+  // Function to handle advancing the game state
   const advanceDispatchHandler = () => {
     dispatch({
       type: ACTIONS.ADVANCE,
@@ -22,40 +23,63 @@ function GameBoardBox({ boxId, boxHolder }) {
     });
   };
 
+  // Determine the class names for the main div
+  const boxClassNames = clsx(
+    "relative flex items-center justify-center flex-grow border-2 basis-[100%]",
+    {
+      // Class for the Winner's boxes
+      "scale-105 rounded-xl duration-1000": winner && winner === boxHolder,
+      // Class for the rest of the box when the winner found
+      "scale-75 duration-1000 rounded-xl opacity-30":
+        winner && winner !== boxHolder,
+      // Fade the moving boxes
+      "opacity-50":
+        helper &&
+        !winner &&
+        (moves.x.length > 2 || moves.o.length > 2) &&
+        (boxId === lastX || boxId === lastO),
+      // Disabled the empty box when Winner is Found
+      "bg-tertiary border-tertiary text-tertiary hover:bg-tertiary hover:text-tertiary":
+        !boxHolder && winner,
+      // Box Style if the Holder is X
+      "bg-quaternary border-quaternary": boxHolder === "x",
+      // Box Style if the Holder is O
+      "bg-septenary border-septenary text-primary": boxHolder === "o",
+      // Placeholder for X's turn
+      "border-quaternary hover:bg-quaternary": !boxHolder && xNext,
+      // Placeholder for O's turn
+      "border-septenary hover:bg-septenary text-primary": !boxHolder && !xNext,
+      // General Style for all boxes
+      "group cursor-pointer transition-all": true,
+    }
+  );
+
+  // Determine the class names for the inner div
+  const innerDivClassNames = clsx(
+    "absolute inset-0 flex items-center justify-center text-5xl valorax",
+    { "opacity-0": !boxHolder, "group-hover:opacity-100 transition-all": true }
+  );
+
+  // Determine the class names for the move icon span
+  const moveIconSpanClassNames = clsx(
+    "absolute flex items-center justify-center text-sm top-2 right-2",
+    {
+      hidden: !helper,
+      "opacity-100": boxId === lastX || boxId === lastO,
+      "opacity-0": boxId !== lastX && boxId !== lastO,
+    }
+  );
+
   return (
-    <div
-      className={`relative flex items-center justify-center flex-grow border-2 basis-[100%] ${
-        !boxHolder
-          ? winner
-            ? "bg-tertiary border-tertiary text-tertiary hover:bg-tertiary hover:text-tertiary"
-            : ""
-          : ""
-      } ${
-        boxHolder
-          ? boxHolder === "x"
-            ? "bg-quaternary border-quaternary"
-            : "bg-septenary border-septenary text-primary"
-          : xNext
-          ? "border-quaternary hover:bg-quaternary"
-          : "border-septenary hover:bg-septenary text-primary"
-      } group cursor-pointer transition-all`}
-      onClick={advanceDispatchHandler}
-    >
-      <div
-        className={`absolute inset-0 flex items-center justify-center text-5xl valorax ${
-          !boxHolder && "opacity-0"
-        } group-hover:opacity-100 transition-all`}
-      >
+    <div className={boxClassNames} onClick={advanceDispatchHandler}>
+      <div className={innerDivClassNames}>
+        {/* Display X or O based on the state */}
         {!boxHolder ? (xNext ? "X" : "O") : boxHolder.toUpperCase()}
-        {moves.x.length > 2 || moves.o.length > 2 ? (
-          <span
-            className={`absolute flex ${
-              !helper && "hidden"
-            } items-center justify-center text-sm top-2 right-2 ${
-              boxId === lastX || boxId === lastO ? "opacity-100" : "opacity-0"
-            }`}
-          >
-            <RiDeleteBin7Fill />
+        {winner ? (
+          ""
+        ) : moves.x.length > 2 || moves.o.length > 2 ? (
+          <span className={moveIconSpanClassNames}>
+            <IoMdMove />
           </span>
         ) : (
           ""
